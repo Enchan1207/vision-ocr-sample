0ece181b7fe082045fc1a2b00df3a1d288046bf0
enchan <enchant0130@gmail.com>
2026-01-20 22:25:59 +0900

    Initial Commit

---
+++ vision-ocr-sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vision-ocr-sample
+//
+//  Created by enchantcode on 2026/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vision-ocr-sample/VisionOcrSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOcrSampleApp.swift
+//  vision-ocr-sample
+//
+//  Created by enchantcode on 2026/01/20.
+//
+
+import SwiftUI
+
+@main
+struct VisionOcrSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
